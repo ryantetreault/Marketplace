420f59227f260096df29d16a3db48452a16b39b5
Holden Brown <holdenbrown28@gmail.com>
2025-04-10 18:18:24 -0400

    Added login and sign up screen

--- marketplace_frontend/src/main/java/com/cboard/marketplace/marketplace_frontend/HelloApplication.java
+++ marketplace_frontend/src/main/java/com/cboard/marketplace/marketplace_frontend/HelloApplication.java
@@ -3,7 +3,9 @@ package com.cboard.marketplace.marketplace_frontend;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -15,9 +17,11 @@ public class HelloApplication extends Application
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.setFill(Color.TRANSPARENT);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.show();
     }
 
