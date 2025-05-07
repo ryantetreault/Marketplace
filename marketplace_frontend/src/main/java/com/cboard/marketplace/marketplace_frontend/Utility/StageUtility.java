89600a3184f09a933a159e5d3ed7704e35910983
Holden Brown <holdenbrown28@gmail.com>
2025-05-07 18:30:55 -0400

    Formatting

--- marketplace_frontend/src/main/java/com/cboard/marketplace/marketplace_frontend/Utility/StageUtility.java
+++ marketplace_frontend/src/main/java/com/cboard/marketplace/marketplace_frontend/Utility/StageUtility.java
@@ -18,34 +18,20 @@ public enum StageUtility
         stage.setHeight(STAGE_HEIGHT);
         stage.show();
     }
+
     public static Stage switchStageTransparent(Stage oldStage)
     {
         oldStage.hide();
-
-
         Stage newStage = new Stage(StageStyle.TRANSPARENT);
-
-
         newStage.setScene(oldStage.getScene());
-
         return newStage;
-
-
     }
+
     public static Stage switchStageDecorated(Stage oldStage)
     {
         oldStage.hide();
-
-
         Stage newStage = new Stage(StageStyle.DECORATED);
-
-
         newStage.setScene(oldStage.getScene());
-
-
-
         return newStage;
-
-
     }
 }
